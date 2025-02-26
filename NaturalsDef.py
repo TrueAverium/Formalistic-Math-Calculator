f06a9081ff6dfc56c74a784b397488da16792ac8
Averium <104313451+TrueAverium@users.noreply.github.com>
2025-02-26 17:44:07 -0500

    Update NaturalsDef.py

--- NaturalsDef.py
+++ NaturalsDef.py
@@ -1,11 +1,6 @@
-#Assumption 1: Pure nothingness cant exist because talking about nothing implies something: nothing
-#Assumption 2: Functions exist 
-#Def of functions: Take input(s) and produce an output, existing by declaration since i need SOMETHING
-#Only 1 assingment is allowed, for declaration of the number dictionary (For my own sanity of labeling)
 import inflect
 
 universe = {}
-
 #Defining zero as the empty set
 def zero():
     return []
@@ -21,9 +16,33 @@ def is_equal(a, b):
     if not a or not b:
         return False
     
-    return is_equal(a[1:], b[1:])
+    return is_equal(predecessor(a), predecessor(b))
 
-#Counting
+#returns true if a > b
+def is_greater(a, b):
+    #If both are equal
+    if not a and not b:
+        return False
+    #If a is greater
+    if a and not b:
+        return True
+    #if b is greater
+    if not a and b:
+        return False
+    return is_greater(predecessor(a), predecessor(b))
+
+#returns if a < b
+def is_less(a, b):
+    if not a and not b:
+        return False
+    if a and not b:
+        return False
+    if not a and b:
+        return True
+    return is_less(predecessor(a), predecessor(b))
+
+
+#Counting w/ successor function where successor(n) = n + 1 in traditional math
 def successor(n):
     if "successor" in universe:
         del universe["successor"]
@@ -31,17 +50,27 @@ def successor(n):
     universe["successor"].append(n) 
     return universe["successor"]
 
-#Assignment replacement
+
+#Assignment replacements
+#Labels the value and does nothing else
 def label(value, label, group):
     group.update({label: value}) 
 
+#Labels the value and adds to specified sub-list within universe
+#Value will exist both in the universe group AND the list, used for countTo
+def labelAppend(value, name, group, List):
+    label(value, name, group)
+    group[List].append(group[name])
+
+#Adding function, add if a & b != 0, then add(a, S(b)) = S(add(a, b)) where S is successor
 def add(a, b):
     # Base case: If b is zero (i.e., the empty set), return a (no change)
     if not b:
         return a
     # Recursive case: Apply successor to a and decrement b
-    return add(successor(a), b[1:])
+    return add(successor(a), predecessor(b))
 
+#Multiply function where if A & B != 0, multiply(a, add(a,b)) = add(multiply(a, b))
 def multiply(a, b):
     # Base case: If b is zero, return zero
     if not b:
@@ -50,6 +79,7 @@ def multiply(a, b):
     # Recursive case: Add a to the result of multiply(a, b - 1)
     return add(a, multiply(a, b[1:]))
     
+    #Defines and labels all numbers up to the given parameter, assuming the parameter IS a labled number in numberlist
 def countTo(number):
     for element in universe["numberlist"]:
         if is_equal(element, number):
@@ -63,13 +93,14 @@ def countTo(number):
         universe['numberlist'].append(successor(element))
         #print(is_equal(universe,))
 
-
+#prints all elements in universe that isnt successor label or numberlist label
 def listNumbers():
     for element in universe:
         if element in {"successor"} or element in {"numberlist"}:
             continue
         print(element)
 
+#Predecessor (P) of n is as follows, P(S(x)) = x, where n = S(x) or P(n) = n -1
 def predecessor(n):
     if "predecessor" in universe:
         del universe["predecessor"]
@@ -77,37 +108,12 @@ def predecessor(n):
     universe["predecessor"].pop() 
     return universe["predecessor"]
 
-#Use predecessor to subtract. ONLY WORKS FOR NATURALSz
-def subtract(a, b):
-    print(str(len(a)) + " " + str(len(b)))
-    # Base case: If b is zero (i.e., the empty set), return a (no change)
-    if not b:
-        return a
-    
-    # Recursive case: Apply predecessor to a and decrement b
-    return subtract(predecessor(a), predecessor(b))
-
 label(zero(), "numberlist", universe)
 #For reference, numberlist contains all VALUES, the universe dictionary contains the VALUES AND LABELS
 
+labelAppend(successor(zero()), "one", universe, "numberlist")
+labelAppend(successor(universe["one"]), "two", universe, "numberlist")
+
+listNumbers()
 
 
-label(successor(zero()), "one", universe) 
-universe["numberlist"].append(universe["one"])
-
-label(successor(universe["one"]), "two", universe)
-universe["numberlist"].append(universe["two"])
-
-label(successor(universe["two"]), "three", universe)
-universe["numberlist"].append(universe["three"])
-
-label(successor(universe["three"]), "four", universe)
-universe["numberlist"].append(universe["four"])
-
-label(multiply(universe["four"], universe["four"]), "sixteen", universe)
-universe["numberlist"].append(universe["sixteen"])
-
-label(multiply(universe["sixteen"], universe["sixteen"]), "two hundred and fifty-six", universe)
-universe["numberlist"].append(universe["two hundred and fifty-six"])
-
-countTo(universe["two hundred and fifty-six"])
